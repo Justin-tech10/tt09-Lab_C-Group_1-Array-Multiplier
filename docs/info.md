364185846b8081b5c145f07db199ef146437a6b7
Justin Alexander Morris <jam9787@ad.nyu.edu>
2024-10-25 10:35:34 -0400

    added files

--- docs/info.md
+++ docs/info.md
@@ -10,10 +10,12 @@ You can also include images in this folder and reference them in the markdown. E
 ## How it works
 
 Explain how your project works
+This projects works, where it takes a 8bit binary number that splits into 4 bit valuse to multiply them together to get a product. 
 
 ## How to test
 
 Explain how to use your project
+This project is to test the product of multiplying 4 bit numbers, to test this we have to give the projects a 8 bit values to then have 4x4 binary product. 
 
 ## External hardware
 
--- src/array_mult_structural.v
+++ src/array_mult_structural.v
@@ -1,23 +1,27 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/18/2024 11:05:11 AM
+// Design Name: 
+// Module Name: array_mult_structural
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-`default_nettype none
-
-module tt_um_array_mult_structural (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
-    wire[3:0] m=ui_in[7:4];
-    wire[3:0] q=ui_in[3:0];
-    wire[7:0] p;
+module array_mult_structural(m,q,p);
+    input [3:0] m,q;
+    output [7:0] p;
     wire[3:0] w1, w2, w3, w4, partial1, partial2, partial3;
     wire[2:0] C;
     
@@ -31,14 +35,8 @@ module tt_um_array_mult_structural (
     add_4bit stage2 (w4,{C[1], partial2[3:1]},partial3,C[2]);
     
     assign p = {C[2], partial3, partial2[0], partial1[0],w1[0]};
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = p
-  assign uio_out = 0;
-  assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n,uio_in, 1'b0};
-
+    
 endmodule
 
 module add_4bit(x,y,z,carry_out);
